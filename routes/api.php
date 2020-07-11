f40850d568851e1fa3c441668b0dfda397e2878b
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-11 20:11:33 -0300

    Building Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/ping', function (Request $request) {
+    return ['pong'=>true];
+});
+
+
+Route::get('/notes', '');
+Route::get('/note`/{id}', '');
+Route::post('/note', '');
+Route::put('/note/{id}', '');
+Route::delete('/note/{id}', '');
