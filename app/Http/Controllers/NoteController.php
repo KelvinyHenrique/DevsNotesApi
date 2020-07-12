c0c08adcced240669b9638979efbf5d45b73a2fc
Kelviny H <kelvinyhenrique17@gmail.com>
2020-07-11 21:27:23 -0300

    Building controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Note;
+
+
+class NoteController extends Controller
+{
+    private $array = ['error'=>'', 'result'=>[]];
+
+    public function all() {
+      $notes = Note::all();
+
+      foreach($notes as $note){
+          $this->array['result'][] = [
+            'id'=>$note->id,
+            'title'=>$note->title,
+            'body'=>$note->body
+          ];
+        }
+        return $this->array;
+    }
+    public function one($id){
+        $note = Note::find($id);
+
+        if($note){
+            $this->array['result'] = $note;
+        } else {
+            $this->array['error'] = 'ID não encontrado';
+        }
+        return $this->array;
+    }
+
+    public function new(Request $request){
+        $title = $request->input('title');
+        $body = $request->input('body');
+
+        if($title && $body){
+            $note = new Note();
+            $note->title = $title;
+            $note->body = $body;
+            $note->save();
+
+            $this->array['result'] = [
+                'id' => $note->id,
+                'title' => $title,
+                'body' => $body
+            ];
+
+        } else {
+            $this->array['error'] = 'Campos não enviados';
+        }
+        return $this->array;
+    }
+
+    public function edit(Request $request, $id){
+        $title = $request->input('title');
+        $body = $request->input('body');
+
+        if($id && $title && $body){
+            $note = Note::find($id);
+            if($note){
+                $note->title = $title;
+                $note->body = $body;
+                $note->save();
+                $this->array['result'] = [
+                    'id'=>$id,
+                    'title'=>$title,
+                    'body'=>$body,
+                ];
+
+            } else {
+                $this->array['error'] = 'ID inexistente';
+            }
+
+        } else {
+            $this->array['error'] = 'Campos não enviados';
+        }
+
+        return $this->array;
+    }
+
+    public function delete($id) {
+        $note = Note::find($id);
+
+        if($note) {
+            $note->delete();
+        } else {
+            $this->array['error'] = 'ID inexistente';
+        }
+
+        return $this->array;
+
+    }
+}
